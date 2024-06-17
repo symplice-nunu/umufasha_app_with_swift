81a3fbd9bcb4c5747faa960a5d446062db55d48a
Noella Umutoni <noellaumutoni5@gmail.com>
2024-06-17 16:03:24 +0200

    Initial Commit

---
+++ Umufasha_App/Umufasha_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Umufasha_AppApp.swift
+//  Umufasha_App
+//
+//  Created by Symplice on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Umufasha_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
